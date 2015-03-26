1867e484c69dbab4762bd32ad7f41c6bd7c27553
Vesa Vänskä <vesa@vesavanska.com>
2015-03-26 17:05:56 +0200

    Adds back personal configuration

---
+++ modules/people/manifests/applications.pp
@@ -0,0 +1,106 @@
+class people::vesan::applications {
+  # Development
+
+  include gitx::dev
+  include zsh
+  include clojure
+  include dash
+  include iterm2::dev
+  include heroku
+  include imagemagick
+  include ctags
+  include postgresapp
+  include phantomjs
+  include virtualbox
+  include github_for_mac
+  include tmux
+  package { 'reattach-to-user-namespace':
+    ensure => present
+  }
+  include ghostscript
+  package { 's3cmd':
+    ensure => present
+  }
+
+  ruby::version { '2.2.1': }
+
+  # Utilities
+
+  include alfred
+  include caffeine
+  include textexpander
+  include flux
+  include divvy
+  file { "${home}/Library/Preferences/com.mizage.divvy.plist":
+    ensure => link,
+    target => "${home}/Dropbox/Sync/com.mizage.divvy.plist",
+  }
+
+  # Apps
+
+  include firefox
+  include kindle
+  include arq
+  include omnifocus
+  include chrome
+  include chrome::canary
+  include dropbox
+  include onepassword
+  include fluid
+  include transmit
+  include transmission
+  include undercover
+  include spotify
+  include notational_velocity::nvalt
+  include flowdock
+  include skype
+  include vlc
+  class { 'libreoffice':
+    version => '4.2.4',
+  }
+
+  # Tab switching keyboard bindings
+
+  shortcut {
+    "Terminal Next Tab":
+      app  => "com.apple.Terminal",
+      key  => "cmd-alt-\\U2192",
+      menu => "Select Next Tab";
+    "Terminal Previous Tab":
+      app  => "com.apple.Terminal",
+      key  => "cmd-alt-\\U2190",
+      menu => "Select Previous Tab";
+    "Finder Next Tab":
+      app  => "com.apple.finder",
+      key  => "cmd-alt-\\U2192",
+      menu => "Select Next Tab";
+    "Finder Previous Tab":
+      app  => "com.apple.finder",
+      key  => "cmd-alt-\\U2190",
+      menu => "Select Previous Tab";
+    "Iterm Next Tab":
+      app  => "com.googlecode.iterm2",
+      key  => "cmd-alt-\\U2192",
+      menu => "Select Next Tab";
+    "Iterm Previous Tab":
+      app  => "com.googlecode.iterm2",
+      key  => "cmd-alt-\\U2190",
+      menu => "Select Previous Tab";
+    "Adium Next Tab":
+      app  => "com.adiumX.adiumX",
+      key  => "cmd-alt-\\U2192",
+      menu => "Next Chat";
+    "Adium Previous Tab":
+      app  => "com.adiumX.adiumX",
+      key  => "cmd-alt-\\U2190",
+      menu => "Previous Chat";
+    "Safari Next Tab":
+      app  => "com.apple.Safari",
+      key  => "cmd-alt-\\U2192",
+      menu => "Select Next Tab";
+    "Safari Previous Tab":
+      app  => "com.apple.Safari",
+      key  => "cmd-alt-\\U2190",
+      menu => "Select Previous Tab";
+  }
+}
---
+++ modules/people/manifests/git.pp
@@ -0,0 +1,149 @@
+class people::vesan::git {
+  package { 'git-extras':
+    ensure => present
+  }
+
+  git::config::global { 'user.name':
+    value  => 'Vesa Vänskä'
+  }
+  git::config::global { 'user.email':
+    value  => 'vesa@vesavanska.com'
+  }
+  git::config::global { 'github.user':
+    value  => $::boxen_user
+  }
+
+  git::config::global { 'push.default':
+    value  => 'current'
+  }
+  git::config::global { 'gui.fontui':
+    value  => '-family Consolas -size 13 -weight normal -slant roman -underline 0 -overstrike 0'
+  }
+  git::config::global { 'gui.fontdiff':
+    value  => '-family Consolas -size 13 -weight normal -slant roman -underline 0 -overstrike 0'
+  }
+  git::config::global { 'merge.tool':
+    value  => 'opendiff'
+  }
+  git::config::global { 'merge.summary':
+    value  => 'true'
+  }
+  git::config::global { 'branch.autosetupmerge':
+    value  => 'true'
+  }
+  git::config::global { 'diff.color':
+    value  => 'auto'
+  }
+  git::config::global { 'diff.rename':
+    value  => 'copy'
+  }
+  git::config::global { 'pager.color':
+    value  => 'true'
+  }
+  git::config::global { 'status.color':
+    value  => 'auto'
+  }
+  git::config::global { 'color.diff':
+    value  => 'auto'
+  }
+  git::config::global { 'color.status':
+    value  => 'auto'
+  }
+  git::config::global { 'color.branch':
+    value  => 'auto'
+  }
+  git::config::global { 'color.interactive':
+    value  => 'auto'
+  }
+
+  # Aliases
+
+  git::config::global { 'alias.st':
+    value  => 'status'
+  }
+  git::config::global { 'alias.ci':
+    value  => 'commit'
+  }
+  git::config::global { 'alias.co':
+    value  => 'checkout'
+  }
+  git::config::global { 'alias.br':
+    value  => 'branch'
+  }
+  git::config::global { 'alias.di':
+    value  => 'diff'
+  }
+  git::config::global { 'alias.dc':
+    value  => 'diff --cached'
+  }
+  git::config::global { 'alias.ignored':
+    value  => '!git ls-files --others --exclude-standard'
+  }
+  git::config::global { 'alias.unadd':
+    value  => 'reset HEAD'
+  }
+  git::config::global { 'alias.lg':
+    value  => "log --graph --pretty=tformat:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr)%Creset %an' --abbrev-commit --date=relative"
+  }
+  git::config::global { 'alias.up':
+    value  => "!sh -c 'git pull --rebase --prune && git log --pretty=format:\"%Cred%ae %Creset- %C(yellow)%s %Creset(%ar)\" HEAD@{1}..'"
+  }
+  git::config::global { 'alias.amend':
+    value  => 'commit --amend'
+  }
+  git::config::global { 'alias.aa':
+    value  => 'add --all'
+  }
+  git::config::global { 'alias.head':
+    value  => '!git l -1'
+  }
+  git::config::global { 'alias.ra':
+    value  => '!git r --all'
+  }
+  git::config::global { 'alias.ff':
+    value  => 'merge --ff-only'
+  }
+  git::config::global { 'alias.pullff':
+    value  => 'pull --ff-only'
+  }
+  git::config::global { 'alias.la':
+    value  => '!git l --all'
+  }
+  git::config::global { 'alias.div':
+    value  => 'divergence'
+  }
+  git::config::global { 'alias.gn':
+    value  => 'goodness'
+  }
+  git::config::global { 'alias.gnc':
+    value  => 'goodness --cached'
+  }
+  git::config::global { 'alias.fa':
+    value  => 'fetch --all'
+  }
+
+  # For some reason these don't work with the normal syntax.
+  exec { "Single letter Git aliases":
+    command =>
+     "git config --global --replace-all alias.l 'log --graph --abbrev-commit --date=relative' \
+      git config --global --replace-all alias.l '!git head'; \
+      git config --global --replace-all alias.l '!git l -20';",
+    path => "${boxen::config::home}/homebrew/bin",
+  }
+
+  exec { "Git Global Settings":
+    command =>
+     "git config --global --replace-all color.\"branch\".current yellow reverse; \
+      git config --global --replace-all color.\"branch\".local yellow; \
+      git config --global --replace-all color.\"branch\".remote green; \
+      git config --global --replace-all color.\"diff\".meta yellow bold; \
+      git config --global --replace-all color.\"diff\".frag magenta bold; \
+      git config --global --replace-all color.\"diff\".old red bold; \
+      git config --global --replace-all color.\"diff\".new green bold; \
+      git config --global --replace-all color.\"status\".added yellow; \
+      git config --global --replace-all color.\"status\".untracked cyan; \
+      git config --global --replace-all color.\"status\".changed green; \
+      git config --global --replace-all diff.\"ruby\".funcname '^ *\\(\\(class\\|module\\|def\\) .*\\)'",
+    path => "${boxen::config::home}/homebrew/bin",
+  }
+}
